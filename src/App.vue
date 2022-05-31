2ba03007de360d6ff811a06745fc717e0c9a99a0
TqGISer <540484369@qq.com>
2022-05-31 19:48:36 +0800

    f cm

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <CvP1/>
+  <CvP2/>
+</template>
+
+<script>
+import CvP1 from './components/CvP1.vue'
+import CvP2 from './components/CvP2.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CvP1,
+    CvP2
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
