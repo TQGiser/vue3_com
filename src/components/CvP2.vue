030bf253fe38fc0bb46fee67cf6018cc589e0d41
TqGISer <540484369@qq.com>
2022-05-31 20:59:10 +0800

    "0531"

--- src/components/CvP2.vue
+++ src/components/CvP2.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
-
+        {{msg}}
     </div>
 </template>
 
 <script>
+import {useStore} from 'vuex'
+import {reactive, toRefs} from 'vue'
     export default {
+        setup(){
+            const store = useStore()
+            const state = reactive({
+                    msg:null
+            })
+                
+            state.msg = store.state.value2
+        return {
+
+            ...toRefs(state)
+        }
+        }
         
     }
 </script>
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import {createStore} from 'vuex'
+export default  createStore({
+    state:{
+        value1:'fch',
+        value2:'fwh'
+    }
+})
